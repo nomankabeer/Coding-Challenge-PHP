f1ad6bc533be100b2632ce18467712022b11c9ca
Noman kabeer <nomiarain266@gmail.com>
2019-10-10 02:14:48 +0500

    Find if a string is palindrome or not

---
+++ PalindromeString.php
@@ -0,0 +1,19 @@
+<?php 
+ // Function to find a given string is Palindrome in php
+function PalindromeString($sen) {
+$stringLength = strlen($sen)-1;
+$stringLengthCount = $stringLength;
+$ans = null;
+for($i=0; $i< $stringLength ; $i++){
+	if($sen[$i] != $sen[$stringLengthCount]){
+		$ans = "Not";
+		break;
+	}
+	$stringLengthCount--;
+}
+$sen = "Given string '$sen' is $ans Palindrome";
+return $sen; 
+}   
+// keep this function call here  
+echo PalindromeString("abbcbba");  
+?>
