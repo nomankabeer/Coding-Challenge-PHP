9c72c8214d0ebba8d77c12f567b40ffe32d7e226
Noman Kabeer <nomankabeerr@gmail.com>
2019-10-27 23:50:54 +0500

    FindHighestAndSecondHighestNumberFromUnsortedArray

---
+++ Basic/FindHighestAndSecondHighestNumberFromUnsortedArray.php
@@ -0,0 +1,21 @@
+<?php
+    
+function FindHighestAndSecondHighestNumberFromUnsortedArray($numbers)
+    {
+        $highest_number = 0;
+        $second_highest = 0;
+        foreach ($numbers as $num) {
+            if ($highest_number < $num) {
+                $second_highest = $highest_number;
+                $highest_number = $num;
+            }
+            elseif($second_highest < $num){
+                $second_highest = $num;
+            }
+        }
+        return "Highest number = ".$highest_number . ' And second highest number = ' . $second_highest."\n";
+    }
+    
+echo FindHighestAndSecondHighestNumberFromUnsortedArray([2,3,5,1,22,4,5,6]);
+
+?>
