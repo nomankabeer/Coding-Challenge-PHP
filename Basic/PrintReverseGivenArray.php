84425d3b7d1282fadc710fd7bdd0e28188cb39eb
Noman Kabeer <nomankabeerr@gmail.com>
2019-10-29 03:21:00 +0500

    Reverse And Print Array Of Given Array

---
+++ Basic/PrintReverseGivenArray.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Noman Kabeer
+ * Date: 29-Oct-2019
+ * Time: 1:12 AM
+ * Probles:
+ * Print reverse array of given array
+ */
+function reverseArray($arr){
+	$n = count($arr);
+	for($i=$n;$i>0;$i--){
+		echo $arr[$i-1].' ';
+	}
+}
+
+$arr = [1,2,3,4,5,6,7,8,9];
+reverseArray($arr);
+
+?>
