9d59579d4402dcdb433b8886ff78e035e165e106
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-01 23:37:59 +0500

    FindTargetNumberByAddingTwoNumbersFromGivenArray

--- Basic/FindTargetNumberByAddingTwoNumbersFromGivenArray.php
+++ Basic/FindTargetNumberByAddingTwoNumbersFromGivenArray.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * User: Noman Kabeer
+ * Date: 1-nov-2019
+ * Time: 1:12 AM
+ * Probles:
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9
+ * return [0, 1].
+ */
 function twoSum($nums, $target) {
     foreach($nums as $i_index => $i){
         foreach($nums as $j_index => $j){
